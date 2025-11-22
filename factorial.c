32372de49bde6ad6482c8e8d5a7dc119200c4539
suratthutejaswini13-cpu <suratthutejaswini13@gmail.com>
2025-11-22 22:15:07 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,fact=1;
+	printf("enter any value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		fact =fact*i;
+	}
+	printf("factorial is=%d",fact);
+	return 0;	
+	}
+
+
