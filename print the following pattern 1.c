2ab07bf91aa4cb4ba324a22d726164803099a135
suratthutejaswini13-cpu <suratthutejaswini13@gmail.com>
2025-11-25 14:42:05 +0530

    Add files via upload

---
+++ print the following pattern 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=4;i++)
+	printf("%d",i);
+	printf("\n");
+	for(i=1;i<=3;i++)
+	printf("%d",i);
+	printf("\n");
+	for(i=1;i<=2;i++)
+	printf("%d",i);
+	printf("\n");
+	for(i=1;i<=1;i++)
+	printf("%d",i);
+	printf("\n");
+	return 0;
+}
