d899319b859463191bbcb1ef8387d7a30804729d
suratthutejaswini13-cpu <suratthutejaswini13@gmail.com>
2025-11-25 14:41:11 +0530

    Add files via upload

---
+++ print the following pattern 2.c
@@ -0,0 +1,54 @@
+include <stdio.h>
+#include <stdlib.h>
+int main() 
+{  
+int A[10][10],B[10][10],C[10][10],r1,c1,r2,c2,i,j,k;
+printf("enter A matrix row&column");
+scanf("%d%d",&r1,&c1);
+printf("enter B matrix row&column");
+scanf("%d%d",&r2,&c2);
+if(r1!=c2)
+	1SW {
+printf("matrix multiplication is not possible"); 
+exit(1);
+}
+printf("enter A matrix values");
+for(i=0;i<r1;i++)
+{
+    for(j=0;j<c1;j++)
+    {
+        scanf("%d",&A[i][j]);
+    }
+}
+printf("enter B matrix values");
+for(i=0;i<r2;i++)
+{
+    for(j=0;j<c2;j++)
+{
+    scanf("%d",&B[i][j]);
+}
+for(i=0;i<r1;i++)
+{
+    for(j=0;j<c1;j++)
+    {
+        C[i][j]=0;
+        for(k=0;k<r2;k++)
+        {
+        C[i][j]=C[i][j]+A[i][k]*B[k][j];
+    }
+}
+}
+printf("result matrix is \n");
+for(i=0;i<r1;i++)
+{
+    for(j=0;j<c1;j++)
+    {
+        printf("%d",C[i][j]);
+    }
+    printf("\n");
+}
+????return?0;
+}
+	
+
+
