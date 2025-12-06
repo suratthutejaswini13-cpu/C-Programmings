c38c8e2ef357f189a01a379cbb89cc17d4ad4df1
suratthutejaswini13-cpu <suratthutejaswini13@gmail.com>
2025-12-06 20:00:36 +0530

    Add files via upload

---
+++ factorial prime or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,count=0;
+	printf("enter any number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	 if(n% i== 0);
+	 count ++
+    {
+    	if(count ==2)
+    	printf("it is in prime");
+		else
+		printf("not a prime");
+	     return 0;
+  	 }
